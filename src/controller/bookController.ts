9e5b3d64d8310b9c798ed5d97513340770f54870
John Doe <hamedhasan422421@gmail.com>
2023-07-15 19:13:18 +0300

    Refactor getBookById function
    Refactored the getBookById function to improve readability and error handling. The function now retrieves a book by its ID and returns it as a JSON response. If the book is not found, it returns a 404 error, and for any other internal server error, it returns a 500 error.

--- src/controller/bookController.ts
+++ src/controller/bookController.ts
@@ -10,3 +10,16 @@ export const getAllBooks = async (_req: Request, res: Response) => {
   }
 };
 
+export const getBookById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+    if (!book) {
+      return res.status(404).json({ error: 'Book not found' });
+    }
+    res.json(book);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
